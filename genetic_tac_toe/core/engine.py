e9ccabb62b53aa35e05e778c1a74dbb56973ef34
smitty42 <smith@terminallabs.com>
2014-03-11 22:22:35 -0500

    The engine now more or less supports actually playing a game.

--- genetic_tac_toe/core/engine.py
+++ genetic_tac_toe/core/engine.py
@@ -1,10 +1,4 @@
-
-
-class Human(object):
-    pass
-
-class Computer(object):
-    pass
+import random
 
 class Game(object):
     def __init__(self):
@@ -13,10 +7,62 @@ class Game(object):
             'mid':{'left':'','mid':'','right':''},
             'bot':{'left':'','mid':'','right':''},
             }
+        self.won = False
+        self.draw = False
 
-    def make_move(self, y_pos, x_pos, player_piece='o'):
+    def make_move(self, y_pos, x_pos, player_piece):
         assert self.state[y_pos][x_pos] == ''
+        assert player_piece == 'x' or player_piece == 'o'
         self.state[y_pos][x_pos] = player_piece
+
+    def check_state(self):
+        def all_same(elements):
+            return all(ele == elements[0] for ele in elements)
+        winning_row = filter(lambda row: all_same(self.state[row].values()) ,self.state)
+        ## Check the rows
+        if winning_row:
+            winner = self.state[winning_row[0]]['left']
+            self.won = winner
+
+        ## Check the columns
+        for col in self.state['top'].keys():
+            if all_same(map(lambda x: self.state[x][col], self.state)):
+                winner = self.state['top'][col]
+                self.won = winner
+
+        ## Check a diaganol
+        if all_same([self.state['top']['left'],self.state['mid']['mid'],self.state['bot']['right']]):
+            winner = self.state['mid']['mid']
+            self.won = winner
+
+        ## Check the other diaganol
+        if all_same([self.state['top']['right'],self.state['mid']['mid'],self.state['bot']['left']]):
+            winner = self.state['mid']['mid']
+            self.won = winner
+
+        ## Check for draw
+        if len(filter(lambda x: all(self.state[x].values()),self.state)) == 3 and not self.won:
+            self.draw = True
+
+class Computer(Game):
+    def compute_move(self):
+        if self.won or self.draw:
+            return
+        pos = 1
+        while True:
+            y_pos = random.choice(self.state.keys())
+            x_pos = random.choice(self.state[y_pos].keys())
+            pos = self.state[y_pos][x_pos]
+            if pos == '':
+                break
+
+        self.make_move(y_pos, x_pos, 'x')
+
+class Human(Game):
+    pass
+
+#    def start(self):
+        
         
 
 
